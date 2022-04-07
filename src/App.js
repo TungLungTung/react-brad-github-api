0b3f7d1f8d88b5b88b1b9de7e5439d7967459fac
Tung Le <tunglungtung87@gmail.com>
2022-04-07 21:53:41 +0700

    delete sth to make project clean

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import './App.css';
-
 function App() {
   return (
     <div>
-      <h1 className="text-xl text-white">Hello world</h1>
-      <span className="text-purple-300">Lorem ipsum dolor sit amet.</span>
+      <h1>Hello world</h1>
     </div>
   );
 }

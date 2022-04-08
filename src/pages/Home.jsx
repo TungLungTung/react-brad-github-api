d700db728b1ab43de4af2c34c6604f2ed4eddf96
Tung Le <tunglungtung87@gmail.com>
2022-04-08 11:25:31 +0700

    finish reducers github, all functionaliy, search, clear

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import UserResults from '../components/users/UserResults';
+import UserSearch from '../components/users/UserSearch';
 
 export default function Home() {
   return (
     <>
       {/* Search components */}
+      <UserSearch />
       <UserResults />
     </>
   );
